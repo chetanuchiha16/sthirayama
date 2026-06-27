62b0b8ac82c5eea89c2cbc612ffc9d2000eb98e2
chetanuchiha16 <chetan16ck@gmail.com>
2026-06-27 14:26:22 +0530

    Refactor skiplist module: expose fields in SkipListNode and SkipList, update main function to demonstrate usage

--- engine/src/skiplist.rs
+++ engine/src/skiplist.rs
@@ -1,17 +1,33 @@
-
+#[derive(Debug)]
 pub struct SkipListNode<K, V> {
-    key: K,
-    value: V,
-    next_nodes: Vec<Option<Box<SkipListNode<K, V>>>>
+    pub level: usize,
+    pub key: K,
+    pub value: V,
+    pub next_nodes: Vec<Option<Box<SkipListNode<K, V>>>>,
 }
 
+impl<K, V> SkipListNode<K, V> {
+    pub fn new(level: usize, key: K, value: V) -> Self {
+        Self {
+            key,
+            value,
+            next_nodes: (0..level).map(|_| None).collect(),
+            level,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct SkipList<K, V> {
-    max_level: usize,
-    head: Vec<Option<Box<SkipListNode<K, V>>>>,
+    pub max_level: usize,
+    pub head: Vec<Option<Box<SkipListNode<K, V>>>>,
 }
 
-impl <K, V>SkipList<K, V> {
-    fn new(max_level: usize) -> Self {
-        Self { max_level: max_level, head:  (0..max_level).map(|_| None).collect() }
+impl<K, V> SkipList<K, V> {
+    pub fn new(max_level: usize) -> Self {
+        Self {
+            max_level: max_level,
+            head: (0..max_level).map(|_| None).collect(),
+        }
     }
 }
