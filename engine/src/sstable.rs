6e685d780ace6777874b4b48338cfd13ee8ba23a
chetanuchiha16 <chetan16ck@gmail.com>
2026-07-17 07:31:01 +0530

    add skiplist to the sstableWriter

--- engine/src/sstable.rs
+++ engine/src/sstable.rs
@@ -1,13 +1,22 @@
-use std::fs::{File, OpenOptions};
+use std::{
+    fs::{File, OpenOptions},
+    io,
+};
+
+use crate::skiplist::{self, SkipList};
 
 pub struct SstableWriter {
     file: File,
+    skiplist: SkipList<Vec<u8>, Vec<u8>>,
 }
 
 impl SstableWriter {
-    pub fn new() -> Self {
-        let file = OpenOptions::new().create(true).open("table.sst").unwrap();
-        Self { file }
+    pub fn new(skiplist: SkipList<Vec<u8>, Vec<u8>>) -> Result<Self, io::Error> {
+        let file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("table.sst")?;
+        Ok(Self { file, skiplist })
     }
     pub fn write(&self) {}
 }
