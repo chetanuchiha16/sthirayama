ee120a66a3fb7b478c4ddd42bd69a908427833b1
chetanuchiha16 <chetan16ck@gmail.com>
2026-07-23 12:16:46 +0530

    add size, last key field, can fit method to datablock struct

--- engine/src/sstable/data_block.rs
+++ engine/src/sstable/data_block.rs
@@ -2,22 +2,27 @@ use crate::skiplist::SkipListKV;
 
 pub struct DataBlock {
     kv_list_bytes: Vec<u8>,
-    size: usize
+    pub size: usize,
+    pub last_key: Vec<u8>,
 }
 
 impl DataBlock {
     pub fn new() -> Self {
         Self {
             kv_list_bytes: Vec::new(),
-            size: 0
+            size: 0,
+            last_key: Vec::new(),
         }
     }
 
-    pub fn add(&mut self, kv: SkipListKV<Vec<u8>, Vec<u8>>) {
-        let (len_byte, data_byte) = kv.encode(); // put all bytes from both
+    pub fn add(&mut self, len_byte: [u8; 8], data_byte: &Vec<u8>) {
         self.kv_list_bytes.extend_from_slice(&len_byte);
-        self.kv_list_bytes.extend_from_slice(&data_byte);
+        self.kv_list_bytes.extend_from_slice(data_byte);
 
         self.size += len_byte.len() + data_byte.len()
+    }
+
+    pub fn can_fit(&self, entry_size: usize) -> bool {
+        self.size + entry_size < 4000
     }
 }
