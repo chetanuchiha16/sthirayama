b7dc6cb40d95b8d0ee08ddecb39790d1692547d4
chetanuchiha16 <chetan16ck@gmail.com>
2026-08-20 20:09:10 +0530

    Add benchmark for Engine using Criterion for performance testing

---
+++ engine/benches/engine_bench.rs
@@ -0,0 +1,20 @@
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use engine::engine::Engine;
+
+
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut engine = Engine::new("main").unwrap();
+
+    c.bench_function("get", |b| {
+        b.iter(|| {
+            let key = &"1".as_bytes().to_vec();
+            let value = "2".as_bytes().to_vec();
+            engine.set(black_box(key), black_box(value)).unwrap();
+        });
+    });
+}
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
